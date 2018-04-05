388c463005bd58173f81c8e17142b88c89339d80
Etienne Bélanger <etienne.belanger@gmail.com>
2018-04-04 20:01:06 -0400

    Send messaages to only people in same route

--- public/js/chat.js
+++ public/js/chat.js
@@ -76,7 +76,6 @@ $('#message-form').on('submit', function(e){
     var messageTextBox = $('[name=message]');
     if(messageTextBox.val().length > 0){
         socket.emit('createMessage', {
-            from:'User',
             text: messageTextBox.val()
         }, function(){
             messageTextBox.val('');
